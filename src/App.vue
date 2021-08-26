789bb856087a27331436fc195fa31dd6d668e6d2
Leonardo dos Santos <Leonardo-Sants@github.com>
2021-08-25 22:25:46 -0300

    Criando carrossel da Home

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <HomeComponent></HomeComponent>
+    <FooterComponent></FooterComponent>
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HomeComponent from './components/HomeComponent.vue';
+import FooterComponent from './components/FooterComponent.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  HomeComponent,
+  FooterComponent
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024px"
+      img-height="180px"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+
+    <!-- imagem solda -->
+      <b-carousel-slide>
+          <h1>Soldagem e Corte</h1>
+            <template #img>
+                <img
+                    class="d-block img-fluid w-100"
+                    src= "../assets/imagens/Solda.jpg"
+                    width="1024px"
+                    height="280px"
+                    alt="image slot"
+                >
+            </template>
+      </b-carousel-slide>
+
+    <!-- imagem inspeção -->
+       <b-carousel-slide>
+           <h1>inspeção</h1>
+            <template #img>
+                <img
+                    class="d-block img-fluid w-100"
+                    height="1024"
+                    width="230"
+                    src= "../assets/imagens/Inspecao.jpg"
+                    alt="image slot"
+                >
+            </template>
+      </b-carousel-slide>
+
+    <!-- imagem tecnologia -->
+         <b-carousel-slide>
+             <h1>Tecnologia</h1>
+            <template #img>
+                <img
+                    class="d-block img-fluid w-100"
+                    src= "../assets/imagens/Tecnologia.jpg"
+                    alt="image slot"
+                >
+            </template>
+      </b-carousel-slide>
+
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+  export default {
+      name: 'HomeComponent',
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart(slide) {
+        this.sliding = true
+        return slide
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+        return slide
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
